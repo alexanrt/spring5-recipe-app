852c7a732356335cc05f9c901f56bc7455d8fcb0
Alex <alex.tandberg@gmail.com>
2021-05-09 16:46:20 +0200

    created recipeService.

---
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -0,0 +1,21 @@
+package guru.springframework.controllers;
+
+import guru.springframework.service.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"/recipes"})
+    public String getRecipes(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
+        return "recipe/index";
+    }
+}
